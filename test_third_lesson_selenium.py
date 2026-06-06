eb622005880ed4b0e5ec7a04c356b3614b63c884
DrozdovMD <corvuscorvx@hotmail.com>
2026-06-06 15:42:31 +0300

    add class StudentForm add class methods add test01

--- test_third_lesson_selenium.py
+++ test_third_lesson_selenium.py
@@ -10,33 +10,29 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class StudentForm:
-
-
+    CITY = (By.ID, "city")
+    STATE = (By.ID, "state")
+    PICTURE = (By.ID, "uploadPicture")
     LAST_NAME = (By.ID, "lastName")
     FIRST_NAME = (By.ID, "firstName")
     USER_EMAIL = (By.ID, "userEmail")
     MOBILE_NUMBER = (By.ID, "userNumber")
     BUTTON_SUBMIT = (By.ID, "submit")
-    PICTURE = (By.ID, "uploadPicture")
     CURRENT_ADDRESS = (By.ID, "currentAddress")
-
+    #Локаторы гендер
     GENDER_MALE_LABEL = (By.CSS_SELECTOR, "label[for='gender-radio-1']")
     GENDER_OTHER_LABEL = (By.CSS_SELECTOR, "label[for='gender-radio-3']")
     GENDER_FEMALE_LABEL = (By.CSS_SELECTOR, "label[for='gender-radio-2']")
     GENDER_MALE_INPUT = (By.ID, "gender-radio-1")
     GENDER_OTHER_INPUT = (By.ID, "gender-radio-3")
     GENDER_FEMALE_INPUT = (By.ID, "gender-radio-2")
-
+    #Локаторы хобби
+    HOBBIES_MUSIC_LABEL = (By.CSS_SELECTOR, "label[for='hobbies-checkbox-3']")
     HOBBIES_SPORTS_LABEL = (By.CSS_SELECTOR, "label[for='hobbies-checkbox-1']")
     HOBBIES_READING_LABEL = (By.CSS_SELECTOR, "label[for='hobbies-checkbox-2']")
-    HOBBIES_MUSIC_LABEL = (By.CSS_SELECTOR, "label[for='hobbies-checkbox-3']")
+    HOBBIES_MUSIC_INPUT = (By.ID, "hobbies-checkbox-3")
     HOBBIES_SPORTS_INPUT = (By.ID, "hobbies-checkbox-1")
     HOBBIES_READING_INPUT = (By.ID, "hobbies-checkbox-2")
-    HOBBIES_MUSIC_INPUT = (By.ID, "hobbies-checkbox-3")
-
-    # HOBBIES_READING = (By.ID, "hobbies-checkbox-2")
-    # SUBJECTS_DROPDOWN = (By.ID, "subjectsDropdown")
-    # STATE = (By.ID, "state")
 
     def __init__(self, url, driver):
         self.url = url
@@ -53,40 +49,57 @@ class StudentForm:
     def tear_down_test(self):
         self.driver.quit()
 
-    def get_title(self): #наличие заголовка
-        form_title = self.wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/main/section/h1")))
+    # наличие заголовка
+    def get_title(self):
+        form_title = self.wait.until(
+            EC.visibility_of_element_located((By.XPATH, "/html/body/main/section/h1"))
+        )
         return form_title.text
 
-    def get_sub_title(self): #наличие подзаголовка
-        form_sub_title = self.wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/main/section/div/p")))
+    #наличие подзаголовка
+    def get_sub_title(self):
+        form_sub_title = self.wait.until(
+            EC.visibility_of_element_located((By.XPATH, "/html/body/main/section/div/p"))
+        )
         return form_sub_title.text
 
-    def close_banner(self): #закрытие баннера
-        self.wait.until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'Level up your automation')]")))
-        close_banner_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, """//*[@id="fixedban"]/div/div/button""")))
+    #Закрыть баннер
+    def close_banner(self):
+        self.wait.until(
+            EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'Level up your automation')]"))
+        )
+        close_banner_button = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, """//*[@id="fixedban"]/div/div/button"""))
+        )
         close_banner_button.click()
         self.wait.until(EC.invisibility_of_element(close_banner_button))
 
+    #Ввести имя
     def input_first_name(self, first_name):
         actual_first_name = self.wait.until(EC.element_to_be_clickable(self.FIRST_NAME)) #не нужна * принимает кортеж как один элемент
         actual_first_name.send_keys(first_name)
         self.wait.until(EC.text_to_be_present_in_element_value(self.FIRST_NAME, first_name))
+
+    #Ввести фамилию
     def input_last_name(self, last_name):
         actual_last_name = self.wait.until(EC.element_to_be_clickable(self.LAST_NAME))
         actual_last_name.send_keys(last_name)
         self.wait.until(EC.text_to_be_present_in_element_value(self.LAST_NAME, last_name))
 
+    #Ввести email
     def input_email(self, email):
         actual_email = self.wait.until(EC.element_to_be_clickable(self.USER_EMAIL))
         actual_email.send_keys(email)
         self.wait.until(EC.text_to_be_present_in_element_value(self.USER_EMAIL, email))
 
+    #Выбрать гендер
     def pick_gender(self, label_locator, input_locator):
         actual_gender = self.wait.until(EC.element_to_be_clickable(label_locator))
         actual_gender.click()
         actual_gender_input = self.wait.until(EC.presence_of_element_located(input_locator))
         assert actual_gender_input.is_selected() == True
 
+    #Ввести номер
     def input_number(self, number):
         actual_number = self.driver.find_element(*self.MOBILE_NUMBER)
         actual_number.send_keys(number)
@@ -94,6 +107,7 @@ class StudentForm:
         number_len = len(number_value)
         assert number_len == 10, f"Ошибка, не 10 цифр! Поле должно содержать номер начиная с 9"
 
+    #Указать дату рождения
     def pick_date_of_birth(self):
         actual_date_of_birth = self.driver.find_element(By.ID, "dateOfBirthInput")
         actual_date_of_birth.click()
@@ -107,22 +121,30 @@ class StudentForm:
         month_pick.click()
         month_pick.find_element(By.XPATH, "//option[text()='June']").click()
 
-        day_pick = self.driver.find_element(By.CSS_SELECTOR, ".react-datepicker__day--011:not(.react-datepicker__day--outside-month)")
+        day_pick = self.driver.find_element(
+            By.CSS_SELECTOR, ".react-datepicker__day--011:not(.react-datepicker__day--outside-month)"
+        )
         day_pick.click()
 
         actual_date_of_birth_value = actual_date_of_birth.get_attribute("value")
         expected_date = "11 Jun 1996"
-        assert actual_date_of_birth_value == expected_date, f"Ошибка! Ожидали дату '{expected_date}', но в поле отображается '{actual_date_of_birth_value}'"
+        assert actual_date_of_birth_value == expected_date,\
+            f"Ошибка! Ожидали дату '{expected_date}', но в поле отображается '{actual_date_of_birth_value}'"
 
+    #Выбрать предмет
     def pick_subjects(self):
         subject = self.wait.until(EC.element_to_be_clickable((By.ID, "subjectsInput")))
         subject.send_keys("Physics")
         subject.send_keys(Keys.ENTER)
-        subject_chip = self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="subjectsChips"]/div/span[1]')))
+        subject_chip = self.wait.until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="subjectsChips"]/div/span[1]'))
+        )
         subject_chip_text = subject_chip.text
         expected_subject = "Physics"
-        assert subject_chip_text == expected_subject, f"Ошибка! Ожидали предмет '{expected_subject}', "f"но отображается '{subject_chip_text}'"
+        assert subject_chip_text == expected_subject,\
+            f"Ошибка! Ожидали предмет '{expected_subject}', "f"но отображается '{subject_chip_text}'"
 
+    #Выбрать хобби
     def pick_hobbies(self, hobbies_list):
         assert 0 <= len(hobbies_list) <= 3, f"Можно выбрать максимум три хобби"
         for label_locator, input_locator in hobbies_list:
@@ -131,10 +153,13 @@ class StudentForm:
             actual_hobbies_input = self.wait.until(EC.presence_of_element_located(input_locator))
             assert actual_hobbies_input.is_selected() == True, f"Чекбокс {input_locator} не выбрался"
 
-    def use_scroll_to_submit_button(self):
-        submit_button = self.driver.find_element(By.ID, "submit")
-        self.driver.execute_script("arguments[0].scrollIntoView();", submit_button)
+    #Скролл
+    @staticmethod
+    def use_scroll():
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.execute_script("document.getElementsByTagName('footer')[0].style.display='none';")
 
+    #Создание временного файла
     @staticmethod
     def create_file():
         temp_file_path = os.path.abspath("test_image.jpg")
@@ -142,24 +167,71 @@ class StudentForm:
             f.write("fake image data")
         return temp_file_path
 
+    #Загрузка временного файла
     def upload_file(self, temp_file_path):
         upload_input = self.driver.find_element(*self.PICTURE)
         upload_input.send_keys(temp_file_path)
 
+    #Удаление временного файла
     @staticmethod
     def delite_file():
         if os.path.exists("test_image.jpg"):
             os.remove("test_image.jpg")
 
+    #Ввести адрес
     def input_current_address(self):
         actual_current_address = self.driver.find_element(*self.CURRENT_ADDRESS)
         actual_current_address.send_keys("Полагаю, что это временный адрес!")
         current_address_value = actual_current_address.get_attribute("value")
-        assert current_address_value == "Полагаю, что это временный адрес!", f"Не совпадает! Получили: '{current_address_value}'"
+        assert current_address_value == "Полагаю, что это временный адрес!",\
+            f"Не совпадает! Получили: '{current_address_value}'"
 
+    #Выбрать штат
+    def pick_state(self):
+        state_dropdown = self.wait.until(EC.element_to_be_clickable(self.STATE))
+        state_dropdown.click()
+        state_option = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="stateCity-wrapper"]/div[1]'))
+        )
+        state_option.click()
 
+    #Выбрать город
+    def pick_city(self):
+        city_dropdown = self.wait.until(EC.element_to_be_clickable(self.CITY))
+        city_dropdown.click()
+        city_option = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="stateCity-wrapper"]/div[1]'))
+        )
+        city_option.click()
 
+    #Нажать кнопку
+    def submit_button(self):
+        submit_button = driver.find_element(*self.BUTTON_SUBMIT)
+        driver.execute_script("arguments[0].click();", submit_button)
 
+    #Отображение окна с результатами
+    def check_result_modal(self):
+        modal_title = self.wait.until(
+            EC.visibility_of_element_located((By.ID, "example-modal-sizes-title-lg"))
+        )
+        assert modal_title.text == "Thanks for submitting the form", (
+            f"Заголовок модального окна не совпадает! Получили: '{modal_title.text}'"
+        )
+
+    #Проверка результатов
+    @staticmethod
+    def check_result_data():
+        result_table = driver.find_element(By.CLASS_NAME, "table-responsive")
+        assert "Автомат Автоматов", result_table.text
+        assert "Avtomat@pitonov.com", result_table.text
+        assert "Male", result_table.text
+        assert "1234567890", result_table.text
+        assert "11 Jun 1996", result_table.text  # форма вывода даты может меняться от настроек
+        assert "Physics", result_table.text
+        assert "Sports, Reading", result_table.text
+        assert "test_image.jpg", result_table.text
+        assert "Полагаю, что это временный адрес!", result_table.text
+        assert "NCR Delhi", result_table.text
 
 
     def test01(self):
@@ -204,8 +276,8 @@ class StudentForm:
         self.pick_hobbies(hobbies_list)
         print("Тест прошел успешно.")
 
-        self.use_scroll_to_submit_button()
-        print("Страница прокручена до кнопки отправки формы.")
+        self.use_scroll()
+        print("Страница прокручена до подвала.")
 
         temp_file = self.create_file()
         self.upload_file(temp_file)
@@ -214,55 +286,21 @@ class StudentForm:
         self.input_current_address()
         print("Тест пройден. Адрес указан!")
 
+        self.pick_state()
+        self.pick_city()
+        print("Штат и город выбраны.")
 
+        self.submit_button()
+        print("Кнопка нажата.")
+        self.check_result_modal()
+        print("Результаты совпали!")
+
+        self.delite_file()
+        print("Временный файл удален.")
+        self.tear_down_test()
+        print("Драйвер выключен.")
 
 url = "https://qa-guru.github.io/one-page-form/automation-practice-form.html"
 driver = webdriver.Chrome()
 student_form = StudentForm(url, driver)
 student_form.test01()
-
-# def get_email(self, email):
-#     self.driver.find_element(*self.USER_EMAIL).send_keys(email)
-#
-# def get_picture(self, upload_picture):
-#     self.driver.find_element(*self.PICTURE).send_keys(upload_picture)
-#
-# def get_mobile_number(self, mobile_number):
-#     self.driver.find_element(*self.MOBILE_NUMBER).send_keys(mobile_number)
-#
-# def get_current_address(self, current_address):
-#     self.driver.find_element(*self.CURRENT_ADDRESS).send_keys(current_address)
-#
-# def get_date_of_birth(self):
-#
-#     self.driver.find_element(*self.DATE_BIRTH).click()
-# def get_subjects(self):
-#     self.driver.find_element(*self.SUBJECTS_DROPDOWN).click()
-#
-# def get_gender_male(self):
-#     self.driver.find_elements(*self.GENDER_MALE).click()
-# def get_gender_other(self):
-#     self.driver.find_elements(*self.GENDER_OTHER).click()
-# def get_gender_female(self):
-#     self.driver.find_elements(*self.GENDER_FEMALE).click()
-#
-# def get_hobbies_sport(self):
-#     self.driver.find_element(*self.HOBBIES_SPORT).click()
-# def get_hobbies_music(self):
-#     self.driver.find_element(*self.HOBBIES_MUSIC).click()
-# def get_hobbies_reading(self):
-#     self.driver.find_element(*self.HOBBIES_READING).click()
-#
-# def get_state(self):
-#     self.driver.find_element(*self.STATE).click()
-#
-# def get_submit_button(self):
-#     self.driver.find_element(*self.BUTTON_SUBMIT).click()
-
-
-# def __init__(self, firstName, lastName, gender, user_number):
-    #     self.firstName = firstName
-    #     self.lastName = lastName
-    #     self.gender = gender
-    #     self.user_number = user_number
-
