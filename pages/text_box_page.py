7e1c27e7e51c0ad8bcbf0cf3f91a4db85aea0081
DrozdovMD <corvuscorvx@hotmail.com>
2026-07-09 15:34:14 +0300

    add def wait_for_element_text in base_page add typings in text_box_page and login_page refactoring tests_login_form.py and tests_text_box_form.py

--- pages/text_box_page.py
+++ pages/text_box_page.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 
 
 class TextBoxPage(BasePage):
+    URL = "https://qa-guru.github.io/one-page-form/text-box.html"
+
     EMAIL = (By.CSS_SELECTOR, "#userEmail")
     FULL_NAME = (By.CSS_SELECTOR, "#userName")
     OUTPUT_AREA = (By.CSS_SELECTOR, "#output")
@@ -10,20 +12,26 @@ class TextBoxPage(BasePage):
     CURRENT_ADDRESS = (By.CSS_SELECTOR, "#currentAddress")
     PERMANENT_ADDRESS = (By.CSS_SELECTOR, "#permanentAddress")
 
-    def input_full_name(self, full_name):
+    def open_mine_url(self):
+        self.open_url(self.URL)
+
+    def input_full_name(self, full_name: str):
         self.input_text(self.FULL_NAME, full_name)
 
-    def input_email(self, email):
+    def input_email(self, email: str):
         self.input_text(self.EMAIL, email)
 
-    def input_current_address(self, current_address):
+    def input_current_address(self, current_address: str):
         self.input_text(self.CURRENT_ADDRESS, current_address)
 
-    def input_permanent_address(self, permanent_address):
+    def input_permanent_address(self, permanent_address: str):
         self.input_text(self.PERMANENT_ADDRESS, permanent_address)
 
     def click_submit_button(self):
         self.click_element(self.SUBMIT_BUTTON)
+
+    def scroll_to_data_output(self):
+        self.scroll_to_element(self.OUTPUT_AREA)
 
     def get_actual_data(self):
         return self.get_element_text(self.OUTPUT_AREA)
--- tests/tests_text_box_form.py
+++ tests/tests_text_box_form.py
@@ -1,63 +1,89 @@
-# TODO: сдлеать чтобы строка с open_url не повторялась в тестах
 def test_positive_data(text_box_page):
-    text_box_page.open_url("https://qa-guru.github.io/one-page-form/text-box.html")
-    text_box_page.input_full_name("Avtomat Avtomatov")
+    full_name = "Avtomat Avtomatov"
+
+    text_box_page.open_mine_url()
+    text_box_page.input_full_name(full_name)
     text_box_page.click_submit_button()
-    actual_name = text_box_page.get_actual_data()
-    assert "Avtomat Avtomatov" in actual_name
+    text_box_page.scroll_to_data_output()
+
+    assert full_name in text_box_page.get_actual_data()
 
 
 def test_positive_mail(text_box_page):
-    text_box_page.open_url("https://qa-guru.github.io/one-page-form/text-box.html")
-    text_box_page.input_email("Avtomat@mail.com")
+    email = "Avtomat@mail.com"
+
+    text_box_page.open_mine_url()
+    text_box_page.input_email(email)
     text_box_page.click_submit_button()
-    actual_email = text_box_page.get_actual_data()
-    assert "Avtomat@mail.com" in actual_email
+    text_box_page.scroll_to_data_output()
+
+    assert email in text_box_page.get_actual_data()
 
 
 def test_positive_current_address(text_box_page):
-    text_box_page.open_url("https://qa-guru.github.io/one-page-form/text-box.html")
-    text_box_page.input_current_address("Eto vremenniy address, dom 1, kv 2, ulica 3")
+    current_address = "Eto vremenniy address, dom 1, kv 2, ulica 3"
+
+    text_box_page.open_mine_url()
+    text_box_page.input_current_address(current_address)
     text_box_page.click_submit_button()
-    actual_current_address = text_box_page.get_actual_data()
-    assert "Eto vremenniy address, dom 1, kv 2, ulica 3" in actual_current_address
+    text_box_page.scroll_to_data_output()
+
+    assert current_address in text_box_page.get_actual_data()
 
 
 def test_positive_permanent_address(text_box_page):
-    text_box_page.open_url("https://qa-guru.github.io/one-page-form/text-box.html")
-    text_box_page.input_permanent_address("A eto address propiski, tut tolko gorod dorog")
+    permanent_address = "A eto address propiski, tut tolko gorod dorog"
+
+    text_box_page.open_mine_url()
+    text_box_page.input_permanent_address(permanent_address)
     text_box_page.click_submit_button()
-    actual_permanent_address = text_box_page.get_actual_data()
-    assert "A eto address propiski, tut tolko gorod dorog" in actual_permanent_address
+    text_box_page.scroll_to_data_output()
+
+    assert permanent_address in text_box_page.get_actual_data()
 
 
 def test_email_without_username(text_box_page):
-    text_box_page.open_url("https://qa-guru.github.io/one-page-form/text-box.html")
-    text_box_page.input_email("@mail.com")
+    email = "@mail.com"
+    validation_message = 'Введите часть адреса до символа "@"'
+
+    text_box_page.open_mine_url()
+    text_box_page.input_email(email)
     text_box_page.click_submit_button()
-    email_failed = text_box_page.get_email_validation_message()
-    assert 'Введите часть адреса до символа "@"' in email_failed
+    text_box_page.scroll_to_data_output()
+
+    assert validation_message in text_box_page.get_email_validation_message()
 
 
 def test_email_without_at(text_box_page):
-    text_box_page.open_url("https://qa-guru.github.io/one-page-form/text-box.html")
-    text_box_page.input_email("Avtomatmail.com")
+    email = "Avtomatmail.com"
+    validation_message = 'Адрес электронной почты должен содержать символ "@"'
+    text_box_page.open_mine_url()
+    text_box_page.input_email(email)
     text_box_page.click_submit_button()
-    email_failed = text_box_page.get_email_validation_message()
-    assert 'Адрес электронной почты должен содержать символ "@"' in email_failed
+    text_box_page.scroll_to_data_output()
+
+    assert validation_message in text_box_page.get_email_validation_message()
 
 
 def test_email_without_domain(text_box_page):
-    text_box_page.open_url("https://qa-guru.github.io/one-page-form/text-box.html")
-    text_box_page.input_email("Avtomat@")
+    email = "Avtomat@"
+    validation_message = 'Введите часть адреса после символа "@"'
+
+    text_box_page.open_mine_url()
+    text_box_page.input_email(email)
     text_box_page.click_submit_button()
-    email_failed = text_box_page.get_email_validation_message()
-    assert 'Введите часть адреса после символа "@"' in email_failed
+    text_box_page.scroll_to_data_output()
+
+    assert validation_message in text_box_page.get_email_validation_message()
 
 
 def test_email_with_a_space(text_box_page):
-    text_box_page.open_url("https://qa-guru.github.io/one-page-form/text-box.html")
-    text_box_page.input_email("Avto mat@mail.com")
+    email = "Avto mat@mail.com"
+    validation_message = 'Часть адреса до символа "@" не должна содержать символ " "'
+
+    text_box_page.open_mine_url()
+    text_box_page.input_email(email)
     text_box_page.click_submit_button()
-    email_failed = text_box_page.get_email_validation_message()
-    assert 'Часть адреса до символа "@" не должна содержать символ " "' in email_failed
+    text_box_page.scroll_to_data_output()
+
+    assert validation_message in text_box_page.get_email_validation_message()
